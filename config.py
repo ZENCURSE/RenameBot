d6a74ce97fe875cf65f312177c6c32be0d540dd9
ZENCURSE <manibharathi21062@gmail.com>
2026-09-02 22:17:58 +0530

    Change default LOG_CHANNEL value to an empty string

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config(object):
     START_PIC   = os.environ.get("START_PIC", "")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
     FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
-    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "0")
+    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "")
 
     # wes response configuration     
     WEBHOOK = os.environ.get("WEBHOOK", "True")
